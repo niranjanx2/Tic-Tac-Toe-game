09b9d39ba99ed3822deddb6c404c4a4c029ee5a3
Niranjan <poddarniranjan2@gmail.com>
2024-09-15 13:03:43 +0530

    enabled two way binding, and made the palyer's name editable and now we can save it as well

--- src/components/player.jsx
+++ src/components/player.jsx
@@ -1,11 +1,29 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react';
+export default function Player({initialName, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName)
+    function handleEdit(){
+        setIsEditing((editing) => !editing);
+    }
+
+    function handleChange(event){
+        console.log(event);
+        setPlayerName(event.target.value);
+    }
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    let buttonName = "Edit";
+    if(isEditing){
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>
+        buttonName='Save';
+    }
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editablePlayerName}
                 <span className="player-symol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEdit}>{buttonName}</button>
         </li>
     )
 }
+
