70328bb34bc2192a6cb4ba520b476c07148c2d36
Niranjan <poddarniranjan2@gmail.com>
2024-09-16 11:01:24 +0530

    made grid editable when clicked, but currently adding static value whenever clicked

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,25 @@
+import {useState} from 'react';
 const initialGameBoard = [
     [null,null,null],
     [null,null,null],
     [null,null,null]
-]
+];
 
 export default function GameBoard(){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectBox(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        })
+    }
     return <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                    <button>{playerSymbol}</button>
+                    <button onClick={() => handleSelectBox(rowIndex, colIndex)}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
